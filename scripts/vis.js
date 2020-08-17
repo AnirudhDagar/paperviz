a315142773c82d5fd4e43b51a4396ab60ddb12d7
SaurabhS78 <saurabhsangam78@gmail.com>
2020-08-17 23:51:47 +0530

    Added scripts to data from JSON and populating the graph and dynamically update the render cards

--- scripts/vis.js
+++ scripts/vis.js
@@ -13,14 +13,14 @@ var svg = d3
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 //Read the data
-d3.csv(
-  "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv",
+d3.json(
+  "https://raw.githubusercontent.com/AnirudhDagar/paperviz/dev/extra/iclr_2020_all_sci_bert_v2.json?token=AOK3RGX6PKCG3VMHCIJJERS7IPXMW",
   function (data) {
     // Add X axis
-    var x = d3.scaleLinear().domain([0, 3000]).range([0, width]);
+    var x = d3.scaleLinear().domain([-30, 30]).range([0, width]);
 
     // Add Y axis
-    var y = d3.scaleLinear().domain([0, 400000]).range([height, 0]);
+    var y = d3.scaleLinear().domain([-30, 30]).range([height, 0]);
     svg.append("g").call(d3.axisLeft(y));
     svg.call(
       d3
@@ -44,16 +44,16 @@ d3.csv(
       .selectAll("circle")
       .data(
         data.filter(function (d, i) {
-          return i < 1000;
+          return i;
         })
-      ) // the .filter part is just to keep a few dots on the chart, not all of them
+      )
       .enter()
       .append("circle")
       .attr("cx", function (d) {
-        return x(d.GrLivArea);
+        return x(d.embedding[0]);
       })
       .attr("cy", function (d) {
-        return y(d.SalePrice);
+        return y(d.embedding[1]);
       })
       .attr("r", 1)
       .attr("class", "dot")
@@ -65,7 +65,7 @@ d3.csv(
     function updateChart() {
       extent = d3.event.selection;
       myDots.classed("selected", function (d) {
-        return isBrushed(extent, x(d.GrLivArea), y(d.SalePrice));
+        return isBrushed(extent, x(d.embedding[0]), y(d.embedding[1]));
       });
     }
 
@@ -81,37 +81,20 @@ d3.csv(
     function brushend() {
       if (!d3.event.selection) return;
       var d_brushed = d3.selectAll(".selected").data();
-      console.log(d_brushed);
 
       $("#render").empty();
       for (var i = 0; i < d_brushed.length; i++) {
+        let authorList = "";
+        for (var j of d_brushed[i].authors) {
+          authorList += j + ", ";
+        }
         document.getElementById("render").innerHTML += `<div class="card">
-            <p class="title">ReMixMatch: Semi-Supervised Learning with Distribution Matching and Augmentation Anchoring</p>
-            <p class="author">David Berthelot, Nicholas Carlini, Ekin D. Cubuk, Alex Kurakin, Kihyuk Sohn, Han Zhang, Colin Raffel</p>
+            <p class="title">${d_brushed[i].title}</p>
+            <p class="author">${authorList}</p>
             <div class="img-container"></div>
            </div>`;
+        console.log(d_brushed[i].embedding);
       }
     }
   }
 );
-
-let request = new XMLHttpRequest();
-request.open(
-  "GET",
-  "https://raw.githubusercontent.com/AnirudhDagar/paperviz/master/extra/iclr_2020_all_sci_bert.json?token=AOK3RGTTTS2K4JBQLQJ6FP27HEMPS"
-);
-request.responseType = "json";
-request.send();
-request.onload = function () {
-  const myData = request.response;
-  var coords = [];
-  for (let key in myData) {
-    if (myData.hasOwnProperty(key)) {
-      for (let id in myData[key]) {
-        coords = myData[key][id].embedding;
-        console.log(coords);
-      }
-    }
-    break;
-  }
-};
