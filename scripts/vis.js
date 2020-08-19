1297d8f459e1cbe2b51dbc5fb3a2b08e2163391c
Anirudh Dagar <anirudhdagar6@gmail.com>
2020-08-19 23:57:39 +0530

    [Fix] Use specter embeddings for viz

--- scripts/vis.js
+++ scripts/vis.js
@@ -14,13 +14,13 @@ var svg = d3
 
 //Read the data
 d3.json(
-  "https://raw.githubusercontent.com/AnirudhDagar/paperviz/dev/extra/iclr_2020_all_sci_bert_v2.json?token=AOK3RGX6PKCG3VMHCIJJERS7IPXMW",
+  "https://raw.githubusercontent.com/AnirudhDagar/paperviz/master/extra/iclr_2020_all_embed.json?token=AFUHAF6XEP564DPVQOEN3HK7IWHIS",
   function (data) {
     // Add X axis
-    var x = d3.scaleLinear().domain([-30, 30]).range([0, width]);
+    var x = d3.scaleLinear().domain([-45, 40]).range([0, width]);
 
     // Add Y axis
-    var y = d3.scaleLinear().domain([-30, 30]).range([height, 0]);
+    var y = d3.scaleLinear().domain([-40, 40]).range([height, 0]);
     svg.append("g").call(d3.axisLeft(y));
     svg.call(
       d3
@@ -50,10 +50,10 @@ d3.json(
       .enter()
       .append("circle")
       .attr("cx", function (d) {
-        return x(d.embedding[0]);
+        return x(d.embedding_specter[0]);
       })
       .attr("cy", function (d) {
-        return y(d.embedding[1]);
+        return y(d.embedding_specter[1]);
       })
       .attr("r", 1)
       .attr("class", "dot")
@@ -65,7 +65,7 @@ d3.json(
     function updateChart() {
       extent = d3.event.selection;
       myDots.classed("selected", function (d) {
-        return isBrushed(extent, x(d.embedding[0]), y(d.embedding[1]));
+        return isBrushed(extent, x(d.embedding_specter[0]), y(d.embedding_specter[1]));
       });
     }
 
@@ -93,7 +93,7 @@ d3.json(
             <p class="author">${authorList}</p>
             <div class="img-container"></div>
            </div>`;
-        console.log(d_brushed[i].embedding);
+        console.log(d_brushed[i].embedding_specter);
       }
     }
   }
