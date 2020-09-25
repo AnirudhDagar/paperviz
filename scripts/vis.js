0d0877629c8bbf1718e010e766c8300b84678440
SaurabhS78 <saurabhsangam78@gmail.com>
2020-09-25 17:50:52 +0530

    added tooltip functionality

--- scripts/vis.js
+++ scripts/vis.js
@@ -1,7 +1,7 @@
 // set the dimensions and margins of the graph
 var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-  width = 887 - margin.left - margin.right,
-  height = 454 - margin.top - margin.bottom;
+  width = window.innerWidth * 0.6 - margin.left - margin.right,
+  height = window.innerHeight * 0.8 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3
@@ -10,24 +10,24 @@ var svg = d3
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform", "translate(" + margin.top + ")");
 
 //Read the data
 d3.json(
-  "https://raw.githubusercontent.com/AnirudhDagar/paperviz/dev/extra/iclr_2020_all_sci_bert_v2.json?token=AOK3RGX6PKCG3VMHCIJJERS7IPXMW",
+  "https://raw.githubusercontent.com/AnirudhDagar/paperviz/master/extra/iclr_2020_all_embed.json?token=AOK3RGSHYG367TLMPAYUCNK7OSW7K",
   function (data) {
     // Add X axis
-    var x = d3.scaleLinear().domain([-30, 30]).range([0, width]);
+    var x = d3.scaleLinear().domain([-45, 40]).range([0, width]);
 
     // Add Y axis
-    var y = d3.scaleLinear().domain([-30, 30]).range([height, 0]);
+    var y = d3.scaleLinear().domain([-45, 40]).range([height, 0]);
     svg.append("g").call(d3.axisLeft(y));
     svg.call(
       d3
         .brush() // Add the brush feature using the d3.brush function
         .extent([
           [0, 0],
-          [887, 454],
+          [width, height],
         ])
         .on("start brush", updateChart) // initialise the brush area
         .on("end", brushend)
@@ -50,22 +50,27 @@ d3.json(
       .enter()
       .append("circle")
       .attr("cx", function (d) {
-        return x(d.embedding[0]);
+        return x(d.embedding_specter[0]);
       })
       .attr("cy", function (d) {
-        return y(d.embedding[1]);
+        return y(d.embedding_specter[1]);
       })
-      .attr("r", 1)
+      .attr("r", 4.5)
       .attr("class", "dot")
       .style("fill", "#69b3a2")
-      .style("opacity", 0.5)
-      .style("stroke", "white");
+      .style("opacity", 0.7)
+      .style("stroke", "white")
+      .call(_tooltip);
 
     // Function that is triggered when brushing is performed
     function updateChart() {
       extent = d3.event.selection;
       myDots.classed("selected", function (d) {
-        return isBrushed(extent, x(d.embedding[0]), y(d.embedding[1]));
+        return isBrushed(
+          extent,
+          x(d.embedding_specter[0]),
+          y(d.embedding_specter[1])
+        );
       });
     }
 
@@ -84,17 +89,37 @@ d3.json(
 
       $("#render").empty();
       for (var i = 0; i < d_brushed.length; i++) {
-        let authorList = "";
-        for (var j of d_brushed[i].authors) {
-          authorList += j + ", ";
-        }
         document.getElementById("render").innerHTML += `<div class="card">
             <p class="title">${d_brushed[i].title}</p>
-            <p class="author">${authorList}</p>
+            <p class="author">${d_brushed[i].authors.join(", ")}</p>
             <div class="img-container"></div>
            </div>`;
-        console.log(d_brushed[i].embedding);
+        console.log(d_brushed[i].embedding_specter);
       }
     }
   }
 );
+
+const _tooltip = function _tooltip(selection) {
+  var div = d3
+    .select("#graph")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+  selection
+    .on("mouseover.tooltip", function (d) {
+      div.style("opacity", "1");
+      div
+        .style("left", d3.event.pageX + 10 + "px")
+        .style("top", d3.event.pageY - 100 + "px");
+      document.querySelector(".tooltip").innerHTML = `<div class="tt-card">
+        <p class="title">${d.title}</p>
+        <p class="author">${d.authors.join(", ")}</p>
+        <div class="img-container"></div>
+       </div>`;
+      console.log(d);
+    })
+    .on("mouseout.tooltip", function () {
+      div.style("opacity", "0");
+    });
+};
